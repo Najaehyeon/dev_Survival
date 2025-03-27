e9de216dc4c43553c83f0bf831ad5fb28a6b9bba
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-03-27 15:44:47 +0900

    Feat: 게임 상태 머신 추가
    게임 상태 구분
    - InGame
    - Score
    - Shop

--- Assets/Team/HDH/Scripts/GameManager.cs
+++ Assets/Team/HDH/Scripts/GameManager.cs
@@ -10,17 +10,66 @@ public class GameManager : Singleton<GameManager>
     [field: SerializeField] public int Day { get; private set; }
     [field: SerializeField] public float PassedTime { get; private set; }
 
-    [SerializeField] private TextMeshProUGUI timer;
+    [SerializeField] public UItest uiTest;
 
-    //날짜 조정
-    //시간 흐르게
-    //인게임-결산-샵 이동 로직
-    //미션 시작 상태 관리
+    public GameStateMachine stateMachine;
+
+    private void Start()
+    {
+        stateMachine = new GameStateMachine();
+        stateMachine.Init();
+
+        uiTest.ScoreText.text = Score.ToString();
+        uiTest.MoneyText.text = Money.ToString();
+    }
 
     private void Update()
     {
-        PassedTime += Time.deltaTime;
-        timer.text = PassedTime.FormatTime();
+        stateMachine.Update();
+    }
+
+    /// <summary>
+    /// Day에 1을 더함
+    /// </summary>
+    public void PassDay()
+    {
+        Day++;
+    }
+
+    /// <summary>
+    /// 입력한 시간만큼 PassedTime에 더함
+    /// </summary>
+    /// <param name="time">더할 시간 입력</param>
+    public void PassTime(float time)
+    {
+        PassedTime += time;
+    }
+    /// <summary>
+    /// 시간을 0으로 초기화
+    /// </summary>
+    public void SetTimeZero()
+    {
+        PassedTime = 0f;
+    }
+
+    /// <summary>
+    /// Score에 변동이 있을 때 사용
+    /// </summary>
+    /// <param name="amount">+,-의 점수에 변동 값</param>
+    public void ChangeScore(int amount)
+    {
+        Score = Mathf.Max(0, Score + amount);
+        uiTest.ScoreText.text = Score.ToString();
+    }
+
+    /// <summary>
+    /// Money에 변동이 있을 때 사용
+    /// </summary>
+    /// <param name="amount">+,-의 Money의 변동값</param>
+    public void ChangeMoney(int amount)
+    {
+        Money = Mathf.Max(0, Money + amount);
+        uiTest.MoneyText.text = Money.ToString();
     }
 
 }
---
+++ Assets/Team/HDH/Scripts/States/ShopState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopState : BaseState
+{
+    public ShopState(GameStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        GameManager.Instance.SetTimeZero();
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            stateMachine.ChangeState(stateMachine.inGameState);
+        }
+    }
+}
