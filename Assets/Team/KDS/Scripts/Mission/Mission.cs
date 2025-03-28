314bc2cd54dfbfa79dcc6c97718d11468a1a12e7
dongseok-kim <kdsehdtjr12@gmail.com>
2025-03-28 11:23:24 +0900

    wip: MissionManager재작중 ,Feat: Mission,Feat : MissionController,Feat: MissionTimer
    wip: MissionManager재작중
    ,Feat: Mission 각각의 미션의 부모
    Feat : MissionController 시간마다 미션페이즈로변해 미션을 불러오는 클래스
    Feat: MissionTimer 미션을 실행하고 타임아웃을 체크해주는 버튼

---
+++ Assets/Team/KDS/Scripts/Mission/Mission.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+
+public abstract class Mission : MonoBehaviour
+{
+    [Header("GameScore")]
+    protected int score;
+    protected float stress;
+
+    public bool isGameEnd;
+    public virtual int GetScroe()
+    {
+        Debug.Log(score);
+        return score;
+    }
+
+    public virtual float GetStress()
+    {
+  
+        float stress = 0;
+        if (score > 0) { stress = 5f; }
+        else { stress = 10f; }
+
+        Debug.Log(stress);
+        return stress;
+    }
+    public virtual void GameEnd()
+    {
+        //게임매니저랑 플레이어랑 연동해서 점수랑 스트레스 변환
+        GetScroe();
+        GetStress();
+        isGameEnd = true;
+        MissionManager.Instance.controller.IsAllGameEnd();
+        gameObject.SetActive(false);
+    }
+}
