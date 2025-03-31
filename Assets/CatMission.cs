1803a496e6d8272b3e52cd1a2075c7195b912af4
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-03-31 10:50:47 +0900

    wip: 작업 내용 중간 저장

--- Assets/CatMission.cs
+++ Assets/CatMission.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class CatMission : Mission
 {
     [SerializeField] MissionTimer[] catMissionTimers;
+    private NPCStateMachine stateMachine;
 
     private void Start()
     {
+        stateMachine = GetComponent<NPCStateMachine>();
+        
         foreach (MissionTimer timer in catMissionTimers)
         {
             timer.mission = this;
         }
+    }
+
+    public void SelectCatMission()
+    {
+        stateMachine.CurrentNPCState.TargetDestination = catMissionTimers[UnityEngine.Random.Range(0, catMissionTimers.Length)].transform.position;
     }
     
     
