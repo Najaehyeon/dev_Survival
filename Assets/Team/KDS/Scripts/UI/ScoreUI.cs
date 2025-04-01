e6ec8f9e2efa782190dee4da785afc07b3ce8404
dongseok-kim <kdsehdtjr12@gmail.com>
2025-04-01 12:09:08 +0900

    Feat:ScoreUI연출
    연출삽입

--- Assets/Team/KDS/Scripts/UI/ScoreUI.cs
+++ Assets/Team/KDS/Scripts/UI/ScoreUI.cs
@@ -1,10 +1,26 @@
 using DG.Tweening;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 public class ScoreUI : BaseUI
 {
+    [Header("UI")]
     public GameObject ScorePannel;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI stressText;
+    public Button Shop;
+    
     private RectTransform rect;
+
+    private int score;
+    private int money;
+    private int endmoney;
+    private int stress;
+    private Coroutine coroutine;
+
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
@@ -15,15 +31,54 @@ public class ScoreUI : BaseUI
     {
         RectTransform rect = ScorePannel.GetComponent<RectTransform>();
         rect.anchoredPosition = new Vector2(-1500, 0);
-        rect.DOAnchorPos(new Vector2(0, 0), 1f).SetEase(Ease.OutCubic);
+        score= GameManager.Instance.Score;
+        money = GameManager.Instance.Money;
+        endmoney = money + score;
+        scoreText.text = score.ToString();
+        moneyText.text = money.ToString();
+        stressText.text = "";
+        Shop.interactable = false;
+        rect.DOAnchorPos(new Vector2(0, 0), 1f).SetEase(Ease.OutCubic)
+            .OnKill(() => coroutine = StartCoroutine(StringChange()));
 
     }
 
     public void OnShopButton()
     {
         rect.DOAnchorPos(new Vector2(1500, 0), 1f).SetEase(Ease.InCubic)
-        .OnKill(() => UIManager.Instance.ChangeState(UIState.Shop));
-        GameManager.Instance.stateMachine.ChangeState(GameManager.Instance.stateMachine.shopState);
+        .OnKill(() => {
+            UIManager.Instance.ChangeState(UIState.Shop);
+            GameManager.Instance.stateMachine.ChangeState(GameManager.Instance.stateMachine.shopState);
+        });
+    }
+
+    public IEnumerator StringChange()
+    {
+
+        while (score != 0)
+        {
+            score--;
+            scoreText.text = score.ToString();
+            money++;
+            moneyText.text = money.ToString();
+            yield return new WaitForSeconds(0.02f);
+        }
+        scoreText.text = "0";
+        moneyText.text = endmoney.ToString();
+        StartCoroutine(TypeStress());
+    }
+
+    public IEnumerator TypeStress()
+    {
+        string stress = $"{GameManager.Instance.Stress} -> {GameManager.Instance.Stress / 2}";
+
+        foreach (char letter in stress)
+        {
+            stressText.text += letter;  
+            yield return new WaitForSeconds(0.1f); 
+        }
+
+        Shop.interactable = true;
     }
     protected override UIState GetUIState()
     {
