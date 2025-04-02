92c37ac5106e94b2b0b2774554606488ff7a85e4
dongseok-kim <kdsehdtjr12@gmail.com>
2025-04-02 15:03:34 +0900

    Docs:Debug삭제

--- Assets/02_Scripts/Manager/OpeningSceneManager.cs
+++ Assets/02_Scripts/Manager/OpeningSceneManager.cs
@@ -7,32 +7,33 @@ public class OpeningSceneManager : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("FirstTime 값: " + PlayerPrefs.GetInt(FirstTimeKey, 1));
         if (IsFirstTime())
         {
-            Debug.Log("처음시작입니다");
             ShowOpeningScene();
             SetFirstTimeFlag(false); 
         }
         else
         {
-            Debug.Log("처음시작아니다");
             Destroy(this);
         }
     }
 
+    /// <summary>
+    /// 처음시작인지 판별
+    /// </summary>
     public bool IsFirstTime()
     {
-        Debug.Log("IsFirstTime() 호출 - FirstTime 값: " + PlayerPrefs.GetInt(FirstTimeKey, 1));
         return PlayerPrefs.GetInt(FirstTimeKey, 1) == 1; 
     }
 
+    /// <summary>
+    /// 시작이후 값변경
+    /// </summary>
     private void SetFirstTimeFlag(bool isFirstTime)
     {
         int value = isFirstTime ? 1 : 0;
         PlayerPrefs.SetInt(FirstTimeKey, value);
         PlayerPrefs.Save();
-        Debug.Log("FirstTime 설정: " + value);
     }
 
     private void ShowOpeningScene()
